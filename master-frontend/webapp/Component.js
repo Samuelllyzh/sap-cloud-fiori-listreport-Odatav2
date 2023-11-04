295e5d101d24afde831956c4fc0aff41a74c5ce5
yzh.samuel97@gmail.com <yzh.samuel97@gmail.com>
2023-11-04 14:55:19 +0000

    init

---
+++ master-frontend/webapp/Component.js
@@ -0,0 +1,13 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("masterfrontend.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
+//# sourceMappingURL=Component.js.map
