5ec88eb916b0a14533712e505e9a556fea5edb97
yzh.samuel97@gmail.com <yzh.samuel97@gmail.com>
2023-11-30 05:33:50 +0000

    隐藏copy按钮

---
+++ master-frontend/webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+    "sap/m/MessageToast"
+], function (MessageToast) {
+    'use strict';
+
+    return {
+        onInit: function (oEvent) {
+            // 隐藏copy按钮
+            setTimeout(() => {
+                // 获取按钮对象
+                var oButton = this.byId("listReport-btnCopy");
+
+                // 确保按钮对象存在
+                if (oButton) {
+                    // 设置按钮为不可见
+                    oButton.setVisible(false);
+                    console.log("Button is now hidden.");
+                } else {
+                    console.log("Button not found!");
+                }
+            }, 100);
+        }
+    };
+});
